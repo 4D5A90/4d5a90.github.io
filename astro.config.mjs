8a151ed80c10b55f7123550f26f1cfd5992cc923
4D5A90 <lebretonhugo@gmail.com>
2023-10-03 17:51:33 +0200

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://4D5A90.github.io",
+  integrations: [mdx(), sitemap()],
 });
